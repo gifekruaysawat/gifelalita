c105d04d8995875ca9542051b53406214a34c3b5
gifekruaysawat <36222173+gifekruaysawat@users.noreply.github.com>
2018-02-07 14:35:47 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => 'สวัสดีครัชช'
+					'text' => 'สวัสดีค๊า'
 				];
 			}
 			else if($text == 'test'){
